443a1f9c81cfa72565098e13c050d106475630b3
jbontk <jbontik@gmail.com>
2022-08-06 15:37:38 +0000

    Exercise 1 - extra 3

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,13 +3,13 @@
 
 import * as React from 'react'
 
-const countReducer = (currentState, newState) => newState;
+const countReducer = (currentState, action) => action(currentState);
 
 function Counter({initialCount = 0, step = 1}) {
   const [state, setState] = React.useReducer(countReducer, {count: initialCount});
 
   const {count} = state;
-  const increment = () => setState({count: count + step})
+  const increment = () => setState(currentState => ({count: currentState.count + step}));
   return <button onClick={increment}>{count}</button>
 }
 
