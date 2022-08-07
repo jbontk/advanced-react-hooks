39ed6253303fda45494493e5930a80369b191ed3
jbontk <jbontik@gmail.com>
2022-08-07 18:45:32 +0000

    Exercise 3 - extra 1

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -4,27 +4,32 @@
 import * as React from 'react'
 import {useContext, useState} from "react";
 
-const CountContext = React.createContext({
-    count: 0,
-    setCount: () => {}
-});
+const CountContext = React.createContext();
 
 function CountProvider({children}) {
     const [count, setCount] = useState(0);
 
-    return <CountContext.Provider value={{count, setCount}}>
+    return <CountContext.Provider value={[count, setCount]}>
         {children}
     </CountContext.Provider>;
 }
 
+const useCount = () => {
+    const countContextValue = useContext(CountContext);
+
+    if (!countContextValue) throw Error('useCount hook must be used within a CountProvider');
+
+    return countContextValue;
+}
+
 
 function CountDisplay() {
-  const {count} = useContext(CountContext);
+  const count = useCount()[0];
   return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
-  const {setCount} = useContext(CountContext);
+  const setCount = useCount()[1];
   const increment = () => setCount(c => c + 1)
   return <button onClick={increment}>Increment count</button>
 }
@@ -32,10 +37,10 @@ function Counter() {
 function App() {
   return (
       <div>
-        <CountProvider>
-          <CountDisplay/>
-          <Counter/>
-        </CountProvider>
+          <CountProvider>
+              <CountDisplay/>
+              <Counter/>
+          </CountProvider>
       </div>
   )
 }
